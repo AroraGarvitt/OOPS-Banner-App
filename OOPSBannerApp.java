b6b05be455651a3e003d07c50423c6c94f1a305e
AroraGarvitt <aroragarvit.ga@gmail.com>
2026-02-22 23:54:16 +0530

    OOPS using String.join

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,46 @@
 // UC1 Implementation
 public class OOPSBannerApp{
  public static void main(String[] args){
-        System.out.println("    ***         ***      ******     *****   ");
-        System.out.println("  **   **     **   **    **   **  **       ");
-        System.out.println(" **     **   **     **   **   **   ***     ");
-        System.out.println(" **     **   **     **   ******      ***    ");
-        System.out.println(" **     **   **     **   **             ** ");
-        System.out.println("  **   **     **   **    **              **  ");
-        System.out.println("    ***         ***      **         *****   ");
+        System.out.println(String.join("",
+                "   ***       ",
+                "***      ",
+                "******     ",
+                "***** "));
+
+        System.out.println(String.join("",
+                "  **  **     ",
+                "**  **    ",
+                "**   **    ",
+                "**    "));
+
+        System.out.println(String.join("",
+                " **    **   ",
+                "**    **   ",
+                "**   **     ",
+                "***  "));
+
+        System.out.println(String.join("",
+                " **    **   ",
+                "**    **   ",
+                "******        ",
+                "*** "));
+
+        System.out.println(String.join("",
+                " **    **   ",
+                "**    **   ",
+                "**               ",
+                "**"));
+
+        System.out.println(String.join("",
+                "  **  **     ",
+                "**  **    ",
+                "**          ",
+                "**   **"));
+
+        System.out.println(String.join("",
+                "   ***         ",
+                "***      ",
+                "**         ",
+                "****** "));
     }
 }
